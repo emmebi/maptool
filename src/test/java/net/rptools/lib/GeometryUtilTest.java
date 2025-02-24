63842c058995766f05f103f0f7101aed02eea6ab
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-02-24 00:24:23 -0800

    Add new tests cases for `GeometryUtil.toJtsPolygons()`
    These new test cases focus on extremely precise geometry that includes self intersections with small and large loops,
    interior rings being near or touching exterior rings, and also a test that bounding boxes aren't used as the only
    determinant for whether one ring is contained in another.

--- src/test/java/net/rptools/lib/GeometryUtilTest.java
+++ src/test/java/net/rptools/lib/GeometryUtilTest.java
@@ -94,6 +94,233 @@ public class GeometryUtilTest {
     }
     // endregion
 
+    // region Simple ring with highly precise backstep
+    {
+      final var path = new Path2D.Double();
+      path.moveTo(-50.095238095238095, 1300.0);
+      path.lineTo(-47.56210321998909, 1302.0459935530857);
+      path.lineTo(-47.56210321998924, 1302.0459935530857);
+      path.lineTo(-47.56211853027344, 1302.0460205078125);
+      path.lineTo(-34.0, 1313.0);
+      path.lineTo(-22.296453965448677, 1316.0530989655351);
+      path.lineTo(-22.296453965448904, 1316.0530989655351);
+      path.lineTo(-22.29646110534668, 1316.0531005859375);
+      path.lineTo(-11.0, 1319.0);
+      path.lineTo(-4.618527782440651E-14, 1331.2222222222222);
+      path.lineTo(0.0, 1331.2222222222222);
+      path.lineTo(0.0, 1300.0);
+      path.closePath();
+      final var area = new Area(path);
+      final var polygons =
+          new Polygon[] {
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(-47.56210321998909, 1302.0459935530857),
+                  new Coordinate(-47.56211853027344, 1302.0460205078125),
+                  new Coordinate(-34, 1313.0),
+                  // Bit odd that this points comes first, but it gives good geometry.
+                  new Coordinate(-22.29646110534668, 1316.0531005859375),
+                  new Coordinate(-22.296453965448677, 1316.0530989655351),
+                  new Coordinate(-11.0, 1319.0),
+                  new Coordinate(0, 1331.22222),
+                  new Coordinate(0, 1300),
+                  new Coordinate(-50.095238095238095, 1300.0),
+                  new Coordinate(-47.56210321998909, 1302.0459935530857)
+                }),
+          };
+
+      argumentsList.add(
+          Arguments.of(
+              Named.of("Simple ring with highly precise backstep", area), List.of(polygons)));
+    }
+    // endregion
+
+    // region Geometry with a collapsible tiny polygon.
+    {
+      final var path = new Path2D.Double();
+      // This first polygon is completely normal.
+      path.moveTo(0, 0);
+      path.lineTo(400, 0);
+      path.lineTo(400, 400);
+      path.lineTo(0, 400);
+      path.closePath();
+      // But this one is so tiny it must be eliminated.
+      path.moveTo(-332.37481837239756, 1635.6524985778758);
+      path.lineTo(-332.3748183723977, 1635.652498577876);
+      path.lineTo(-332.37481837239727, 1635.652498577876);
+      path.lineTo(-332.3748183723976, 1635.6524985778758);
+      path.closePath();
+      final var area = new Area(path);
+      final var polygons =
+          new Polygon[] {
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(0, 400),
+                  new Coordinate(400, 400),
+                  new Coordinate(400, 0),
+                  new Coordinate(0, 0),
+                  new Coordinate(0, 400),
+                }),
+          };
+
+      argumentsList.add(
+          Arguments.of(
+              Named.of("Geometry with a collapsible tiny polygon.", area), List.of(polygons)));
+    }
+    // endregion
+
+    // region Ocean contained in island's bounding box but not actually a child of that island.
+    {
+      final var path = new Path2D.Double();
+      // Main big island.
+      path.moveTo(-200, -100);
+      path.lineTo(200, -100);
+      path.lineTo(200, 200);
+      path.lineTo(-200, 200);
+      path.lineTo(-200, 1000);
+      path.lineTo(-500, 1000);
+      path.lineTo(-500, -1000);
+      path.lineTo(-200, -1000);
+      path.closePath();
+      // Cut out an ocean
+      path.moveTo(0, 0);
+      path.lineTo(0, 100);
+      path.lineTo(100, 100);
+      path.lineTo(100, 0);
+      path.closePath();
+      // Smaller island whose bounding box encompasses the above ocean.
+      path.moveTo(-100, -200);
+      path.lineTo(-100, -300);
+      path.lineTo(400, -300);
+      path.lineTo(400, 400);
+      path.lineTo(-100, 400);
+      path.lineTo(-100, 300);
+      path.lineTo(300, 300);
+      path.lineTo(300, -200);
+      path.closePath();
+
+      final var area = new Area(path);
+      final var polygons =
+          new Polygon[] {
+
+            // Second polygon is the smaller island.
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(-100, -200),
+                  new Coordinate(300, -200),
+                  new Coordinate(300, 300),
+                  new Coordinate(-100, 300),
+                  new Coordinate(-100, 400),
+                  new Coordinate(400, 400),
+                  new Coordinate(400, -300),
+                  new Coordinate(-100, -300),
+                  new Coordinate(-100, -200),
+                }),
+
+            // First polygon is the big island with the ocean.
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(-200, 1000),
+                  new Coordinate(-200, 200),
+                  new Coordinate(200, 200),
+                  new Coordinate(200, -100),
+                  new Coordinate(-200, -100),
+                  new Coordinate(-200, -1000),
+                  new Coordinate(-500, -1000),
+                  new Coordinate(-500, 1000),
+                  new Coordinate(-200, 1000),
+                },
+                new Coordinate[] {
+                  new Coordinate(0, 100),
+                  new Coordinate(0, 0),
+                  new Coordinate(100, 0),
+                  new Coordinate(100, 100),
+                  new Coordinate(0, 100),
+                }),
+          };
+
+      argumentsList.add(
+          Arguments.of(
+              Named.of(
+                  "Ocean contained in island's bounding box but not actually a child of that island.",
+                  area),
+              List.of(polygons)));
+    }
+    // endregion
+
+    // region Self-intersection must be handled reasonably
+    {
+      final var path = new Path2D.Double();
+      // A simple bow-tie.
+      path.moveTo(0, 0);
+      path.lineTo(100, 0);
+      path.lineTo(0, 100);
+      path.lineTo(100, 100);
+      path.closePath();
+
+      final var area = new Area(path);
+      final var polygons =
+          new Polygon[] {
+            // Reduces to two triangular polygons.
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(50, 50),
+                  new Coordinate(100, 0),
+                  new Coordinate(0, 0),
+                  new Coordinate(50, 50),
+                }),
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(0, 100),
+                  new Coordinate(100, 100),
+                  new Coordinate(50, 50),
+                  new Coordinate(0, 100),
+                }),
+          };
+
+      argumentsList.add(
+          Arguments.of(
+              Named.of("Self-intersection must be handled reasonably", area), List.of(polygons)));
+    }
+    // endregion
+
+    // region Tiny self-intersection must not affect topological structure
+    {
+      final var path = new Path2D.Double();
+      // A box with a self-intersection along one edge.
+      path.moveTo(0, 0);
+      path.lineTo(100, 0);
+
+      // Introduce a tiny error that causes a self-intersection in the edge of the box.
+      final var error = 1e-6;
+      path.lineTo(100 + error, 50 + error);
+      path.lineTo(100 + error, 50 - error);
+
+      path.lineTo(100, 100);
+      path.lineTo(0, 100);
+      path.closePath();
+
+      final var area = new Area(path);
+      final var polygons =
+          new Polygon[] {
+            // Reduces to a single square.
+            createPrecisePolygon(
+                new Coordinate[] {
+                  new Coordinate(0, 0),
+                  new Coordinate(100, 0),
+                  new Coordinate(100, 100),
+                  new Coordinate(0, 100),
+                  new Coordinate(0, 0),
+                }),
+          };
+
+      argumentsList.add(
+          Arguments.of(
+              Named.of("Tiny self-intersection must not affect topological structure", area),
+              List.of(polygons)));
+    }
+    // endregion
+
     // region Cut and paste back connected areas
     {
       /*
