b5ac4b77933fa1d03cc84030559e8ef5af860332
bubblobill <45483160+bubblobill@users.noreply.github.com>
2025-06-12 16:57:18 +0800

    GridRenderer - takes back the actual drawing of grid shapes. - scales drawing stroke according to zone scale. - bases stroke on size of grid for consistent appearance (2% of size) between grid sizes. - paints grids with a translucent contrast shadow above 50% zoom. Square grid - changed coordinates to have drop shadow on all sides to improve legibility. Square, Iso, and Hex Grid - pass rendering back to grid renderer for actual drawing.

--- src/main/java/net/rptools/maptool/client/ui/zone/renderer/GridRenderer.java
+++ src/main/java/net/rptools/maptool/client/ui/zone/renderer/GridRenderer.java
@@ -14,31 +14,93 @@
  */
 package net.rptools.maptool.client.ui.zone.renderer;
 
+import com.github.weisj.jsvg.util.ColorUtil;
 import java.awt.*;
+import net.rptools.lib.image.ImageUtil;
 import net.rptools.maptool.client.AppState;
 import net.rptools.maptool.client.ui.zone.PlayerView;
-import net.rptools.maptool.model.Zone;
+import net.rptools.maptool.events.MapToolEventBus;
+import net.rptools.maptool.model.*;
 
 public class GridRenderer {
-  private final Zone zone;
-  private final ZoneRenderer renderer;
+  private ZoneRenderer renderer;
+  private Zone zone;
+  private static float gridLineWeight;
+  private static float scale;
+  private static float baseWidth = 2f;
+  private static Color[] gridColours;
 
-  public GridRenderer(ZoneRenderer renderer) {
+  GridRenderer(ZoneRenderer renderer) {
+    new MapToolEventBus().getMainEventBus().register(this);
+    setRenderer(renderer);
+  }
+
+  public void setRenderer(ZoneRenderer renderer) {
     this.renderer = renderer;
+    if (renderer.getZone() == null) {
+      return;
+    }
     this.zone = renderer.getZone();
   }
 
+  private void setGridColours() {
+    Color gc = new Color(zone.getGridColor());
+    Color contrast = new Color(ImageUtil.negativeColourInt(zone.getGridColor()));
+    gridColours =
+        new Color[] {
+          gc,
+          ColorUtil.withAlpha(gc, 0.14f),
+          ColorUtil.withAlpha(contrast, 0.04f),
+          ColorUtil.withAlpha(contrast, 0.05f)
+        };
+  }
+
+  public static void drawGridShape(Graphics2D g, Shape shape) {
+    if (scale > 0.49f) {
+      for (int i = 3; i > -1; i--) {
+        g.setColor(gridColours[i]);
+        g.setStroke(
+            new BasicStroke(
+                baseWidth * (i + 1) * 0.5f * gridLineWeight * scale,
+                BasicStroke.CAP_ROUND,
+                BasicStroke.JOIN_MITER));
+        g.draw(shape);
+      }
+    } else {
+      g.setColor(gridColours[0]);
+      g.setStroke(
+          new BasicStroke(
+              Math.clamp(
+                  baseWidth * gridLineWeight * scale,
+                  baseWidth * gridLineWeight * 0.15f,
+                  baseWidth * gridLineWeight * 0.25f),
+              BasicStroke.CAP_ROUND,
+              BasicStroke.JOIN_MITER));
+      g.draw(shape);
+    }
+  }
+
   public void renderGrid(Graphics2D g, PlayerView view) {
-    int gridSize = (int) (zone.getGrid().getSize() * renderer.getScale());
-    if (!AppState.isShowGrid() || gridSize < ZoneRendererConstants.MIN_GRID_SIZE) {
+    if (zone == null
+        || !AppState.isShowGrid()
+        || zone.getGrid().getSize() * renderer.getScale() < ZoneRendererConstants.MIN_GRID_SIZE) {
+      if (renderer != null) {
+        setRenderer(renderer);
+      }
       return;
+    }
+    gridLineWeight = AppState.getGridLineWeight();
+    scale = (float) renderer.getScale();
+    baseWidth = zone.getGrid().getSize() / 50f;
+    if (gridColours == null) {
+      setGridColours();
     }
     zone.getGrid().draw(renderer, g, g.getClipBounds());
   }
 
   public void renderCoordinates(Graphics2D g, PlayerView view) {
     if (AppState.isShowCoordinates()) {
-      zone.getGrid().drawCoordinatesOverlay(g, renderer);
+      zone.getGrid().drawCoordinatesOverlay(g, this.renderer);
     }
   }
 }
