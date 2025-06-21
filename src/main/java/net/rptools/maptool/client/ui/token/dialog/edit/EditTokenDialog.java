bf81b77d8573130c3a56249e65ace509e22a046c
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2025-06-21 13:58:05 -0700

    Always use TokenPropertyTableModel for properties table
    When initializing, the model is set to an empty TokenPropertyTableModel to override the default model. And when updating
    the table, don't use a DefaultTableModel but use a TokenPropertyTableModel with a null property source (token).

--- src/main/java/net/rptools/maptool/client/ui/token/dialog/edit/EditTokenDialog.java
+++ src/main/java/net/rptools/maptool/client/ui/token/dialog/edit/EditTokenDialog.java
@@ -51,7 +51,6 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.text.Position.Bias;
 import javax.swing.text.html.HTMLDocument;
@@ -136,6 +135,10 @@ public class EditTokenDialog extends AbeillePanel<Token> {
   /** Create a new token notes dialog. */
   public EditTokenDialog() {
     this(new TokenPropertiesDialog());
+  }
+
+  public void initPropertyTable() {
+    getPropertyTable().setModel(new TokenPropertyTableModel());
   }
 
   public void initGMNotesEditorPane() {
@@ -677,15 +680,9 @@ public class EditTokenDialog extends AbeillePanel<Token> {
     EventQueue.invokeLater(
         () -> {
           PropertyTable pp = getPropertyTable();
-          if (token != null) {
-            var propertyList = MapTool.getCampaign().getTokenPropertyList(propertyType);
-
-            pp.setModel(
-                new TokenPropertyTableModel(
-                    token, propertyType, propertyList, propertyCellRenderer));
-          } else {
-            pp.setModel(new DefaultTableModel());
-          }
+          var propertyList = MapTool.getCampaign().getTokenPropertyList(propertyType);
+          pp.setModel(
+              new TokenPropertyTableModel(token, propertyType, propertyList, propertyCellRenderer));
           pp.expandAll();
         });
   }
@@ -2165,8 +2162,13 @@ public class EditTokenDialog extends AbeillePanel<Token> {
     private final List<TokenProperty> propertyList;
     private final Map<String, String> propertyMap;
 
+    public TokenPropertyTableModel() {
+      propertyList = List.of();
+      propertyMap = Map.of();
+    }
+
     public TokenPropertyTableModel(
-        Token model,
+        @Nullable Token model,
         String propertyType,
         List<TokenProperty> propertyList,
         TableCellRenderer propertyCellRenderer) {
@@ -2174,7 +2176,7 @@ public class EditTokenDialog extends AbeillePanel<Token> {
 
       this.propertyMap = new HashMap<>();
       for (TokenProperty property : propertyList) {
-        String value = (String) model.getProperty(property.getName());
+        String value = model == null ? null : (String) model.getProperty(property.getName());
         if (value == null) {
           value = property.getDefaultValue();
         }
